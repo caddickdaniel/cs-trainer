ad46f78b9ce570fbe354de7869ca5d48f9f67fb6
caddickdaniel <caddickdaniel93@gmail.com>
2023-05-17 10:27:26 +0100

    fixed connection file to fix knex client error

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const ENV = process.env.NODE_ENV || 'development';
 const config = ENV === 'production'
   ? { client: 'postgresql', connection: process.env.DATABASE_URL }
-  : require('../knexfile');
+  : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,12 @@
-`module.exports = {
+module.exports = {
   development: {
     client: 'pg',
     connection: {
       host: 'localhost',
       user: 'danny',
       password: 'password',
-      database: 'cs_trainer_test',
+      database: 'cs_trainer',
     },
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-  },
-};`
-
-module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: 'localhost',
-      user: 'danny',
-      password: 'password',
-      database: 'cs_trainer_test',
-    },
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-  },
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations',
     },
